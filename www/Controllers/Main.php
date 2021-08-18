7c2832fbee9a138e142e0ac2894b2a49fc5c75d3
Fasso <fassory.diaby@gmail.com>
2021-08-18 14:34:22 +0000

    add environnement work

---
+++ www/Controllers/Main.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Core\Database;
+use App\Core\View;
+
+class Main
+{
+    //Method : Action
+    public function defaultAction(){
+
+        $pseudo = "Prof"; // Depuis la bdd
+
+
+
+
+
+
+
+    }
+
+    //Method : Action
+    public function page404Action(){
+
+        //Affiche la vue 404 intégrée dans le template du front
+
+    }
+
+
+
+}
